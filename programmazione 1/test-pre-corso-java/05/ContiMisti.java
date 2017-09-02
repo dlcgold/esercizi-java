255882b2a1d1046a4938e38faa78cb1cabd99df0
dlcgold <dlcgold96@gmail.com>
2017-09-02 17:36:56 +0200

    new files

--- programmazione 1/test-pre-corso-java/05/ContiMisti.java
+++ programmazione 1/test-pre-corso-java/05/ContiMisti.java
@@ -5,25 +5,55 @@ public class ContiMisti
 	public static void main(String [] args)
 	{
 		double a, fat, b, c, prod, d, e, pot;
-		System.out.println("Inserire il numero di cui calcolare il fattoriale: ");
+		System.out.print("Inserire il numero di cui calcolare il fattoriale: ");
 		Scanner tastiera=new Scanner(System.in);
 		a=tastiera.nextDouble();
 		fat=fattoriale(a);
 		System.out.println("Il fattoriale di "+a+" è: "+fat);
 
-		System.out.println("Inserire ora due numeri di cui effettuare il prodotto: ");
+		System.out.print("Inserire ora due numeri di cui effettuare il prodotto: ");
 		b=tastiera.nextDouble();
 		c=tastiera.nextDouble();
 		prod=prodotto (b,c);
 		System.out.println("Il prodotto tra "+b+" e "+c+" è: "+prod);
 
-		System.out.println("Inserire ora base e esponente di una potenza: ");
+		System.out.print("Inserire ora base e esponente di una potenza: ");
 		d=tastiera.nextDouble();
 		e=tastiera.nextDouble();
 		pot=potenza(d,e);
 		System.out.println("Il valore di "+d+" ^ "+e+" è: "+pot);
 
+		System.out.print("Inserire quanti numeri divisibili per 3 e per 5 si vogliono vedere a schermo: ");
+		int f=tastiera.nextInt();
+		int conta=1, numeri=0, resto, restoc;
+		while(numeri<=f)
+		{
+			if(conta%3==0)
+			{
+				System.out.println(conta+" che è divisibile per 3");
+				conta++;
+			}
+			else if (conta%5==0) 
+			{
+				System.out.println(conta+" che è divisibile per 5");
+				conta++;
+			}
+			else if(conta%3!=0)
+			{
+				numeri--;
+				conta++;
+			}
+			else if(conta%5!=0)
+			{
+				numeri--;
+				conta++;
+			}
+			numeri ++;	
+		
+		}
 	}
+
+
 
 	static double fattoriale(double a)
 	{
